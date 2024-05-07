5126694fc5327d322e076e19815e0deafbd10a3f
Your Name <saul.vera787@gmail.com>
2024-05-07 17:21:51 -0400

    modifying output to match desired length

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
+word = "School"
 
 print(f"First 3 letters: " + word[0:3])
 
 print(f"Last 2 letters: " + word[-2:])
 
-print(f"Middle word: " + word[1:8])
+print(f"Middle word: " + word[1:5])

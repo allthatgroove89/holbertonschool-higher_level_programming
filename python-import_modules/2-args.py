2cc9f399e4c0fe064f57ee2373c860481e582ecd
Your Name <saul.vera787@gmail.com>
2024-05-10 15:54:22 -0400

    Dynamic script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print(".")
+    else:
+        arg_word = "argument" if len(args) == 1 else "arguments"
+        print(str(len(args)) + ' ' + arg_word + ":")
+        for i, arg in enumerate(args, start=1):
+            print(str(i) + ":", arg)
+
+
+if __name__ == "__main__":
+    main()

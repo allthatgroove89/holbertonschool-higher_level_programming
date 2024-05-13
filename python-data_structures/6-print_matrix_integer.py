c962b2b3ce3a830205ef31af5fb5e5f226e9024e
Your Name <saul.vera787@gmail.com>
2024-05-13 16:01:02 -0400

    changing print format to str.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         if matrix[i]:
-            print(' '.join(map(str, matrix[i])), end='')
-        if i != len(matrix) - 1 and i + 1 < len(matrix):
-            print('$')
-        else:
-            print('$')
-    else:
-        print('$')
+            print("{}".format(' '.join(map(str, matrix[i]))))
+    if not matrix or not matrix[-1]:
+        print('')

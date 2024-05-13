62512b3a3f791ea91f1edf34e77c6b9bcc17ccc0
Your Name <saul.vera787@gmail.com>
2024-05-13 16:06:59 -0400

    changing print format to str.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         if matrix[i]:
-            print("{}".format(' '.join(map(str, matrix[i]))))
+            print("{}".format(' '.join(map(str, matrix[i]))), end='')
+            print('')
     if not matrix or not matrix[-1]:
         print('')

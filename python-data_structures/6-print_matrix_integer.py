95541eaca133d8dbad4c5bbe8c10dd7bf90723c1
Your Name <saul.vera787@gmail.com>
2024-05-13 19:09:01 -0400

    changing code syntax

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        if matrix[i]:
-            print("{}".format(' '.join(map(str, matrix[i]))), end='')
-            print('')
-    if not matrix or not matrix[-1]:
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end='' if i == len(row) - 1 else ' ')
         print('')

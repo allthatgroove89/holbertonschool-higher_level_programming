d314dcaf5ce4f19ba65de6a060e578565b4d9b5c
Your Name <saul.vera787@gmail.com>
2024-05-17 15:23:04 -0400

    Documenting my module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,5 +9,7 @@ This module demosntrates the creation of an empty class in Python.
 
 
 class Square:
-    """This is an empty class that defines a square."""
     pass
+
+
+"""This is an empty class that defines a square."""

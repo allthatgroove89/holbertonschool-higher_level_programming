b06f3046261e14508aac5c3d65f989337a642321
Your Name <saul.vera787@gmail.com>
2024-05-17 15:49:10 -0400

    adding private instance attribute and instantiation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+    This module defines a class Square.
+
+    This module demonstrates the creation of a cass in Python that initializes
+    a private instance attribute: size.
+    """
+
+
+class Square:
+    """
+    This is a class that defines a square.
+
+    Attributes:
+         __size (int): size of the side of the square.
+    """
+    def __init__(self, size):
+        """
+        The constructor for Square class.
+
+        Parameters:
+            size (int): size of the side of the square.
+
+        """
+        self.__size = size

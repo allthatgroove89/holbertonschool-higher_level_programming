9a8b175469d69650a84146f9c6c8168e8483da76
Your Name <saul.vera787@gmail.com>
2024-05-17 16:36:15 -0400

    Adding Documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square.
+The Square class defines a square by its size, which defaults to 0 if not provided.
+"""
+
+
 class Square:
+    """
+    A class that defines a square by its size.
+
+    ...
+    Attributes
+    ----------
+    size : int
+        size of the square, default to 0
+    Methods
+    -------
+    __init__(self, size=0):
+        Initalizes the square
+    """
     def __init__(self, size=0):
+        """
+        Constructs all the necessary attributes for the square object.
+
+        Parameters
+        ----------
+            size : int
+                size of the square, defaults to 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:

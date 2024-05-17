494b941ef4289d5e623925cc8b402f03b5253dfe
Your Name <saul.vera787@gmail.com>
2024-05-17 17:08:43 -0400

    Area of square with private and public intance method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+Square: A class that defines a square by its size
+and can calculate its area.
+"""
+
+
+class Square:
+    """
+    Square class with size attribute and area method.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize square with size. Size must be an integer
+        and >= 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = __size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+        """
+        return self.__size ** 2

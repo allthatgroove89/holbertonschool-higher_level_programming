2db46516562fe70f0b82a145d14e42236dfa4f8a
Your Name <saul.vera787@gmail.com>
2024-05-17 23:11:18 -0400

    changing syntax

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
-    """Represents a square."""
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes the square with a size and position."""
-        self.__size = size
-        self.__position = position
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Gets the size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            """Sets the size of the square."""
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
-        """Gets the position of the square."""
-        return self.__position
+        """Get/set the current position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        """Sets the position of the square."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -36,12 +43,11 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Returns the area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Prints the square with the character #
-        at the position specified by the position attribute."""
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return

769b70d16e9e3dbe93f44dec6f8bcd8cfa065303
Your Name <saul.vera787@gmail.com>
2024-05-20 18:50:33 -0400

    modifying syntax

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,19 +8,19 @@ Args: matrix, div
 
 def matrix_divided(matrix, div):
     """Divide a Matrix by two """
-    if type(matrix) is not list:
+    if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of\
                 integers/floats")
     size = None
     for row in matrix:
-        if type(row) is not list:
+        if not isinstance(row, list):
             raise TypeError("matrix must be a matrix (list of lists) of\
                     integers/floats")
         if size is None:
             size = len(row)
         elif size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-    if type(div) not in [int, float]:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")

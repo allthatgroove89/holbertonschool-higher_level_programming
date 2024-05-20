54cf13ae53ea19107d04d3636dfa3d44a61e5386
Your Name <saul.vera787@gmail.com>
2024-05-20 18:47:19 -0400

    modifying syntax

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,14 +4,16 @@ Task: 2-matrix_divided
 Test: Cases for unittest
 Args: matrix, div
 """
+
+
 def matrix_divided(matrix, div):
     """Divide a Matrix by two """
-    if type(matrix) != list:
+    if type(matrix) is not list:
         raise TypeError("matrix must be a matrix (list of lists) of\
                 integers/floats")
     size = None
     for row in matrix:
-        if type(row) != list:
+        if type(row) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of\
                     integers/floats")
         if size is None:
@@ -19,7 +21,7 @@ def matrix_divided(matrix, div):
         elif size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
     if type(div) not in [int, float]:
-            raise TypeError("div must be a number")
+        raise TypeError("div must be a number")
     if div == 0:
-            raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero")
     return [[round(element / div, 2) for element in row] for row in matrix]

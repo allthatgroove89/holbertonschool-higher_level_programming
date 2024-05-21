3a9b00f455f96138a6c2751574d6221b93aa89a2
Your Name <saul.vera787@gmail.com>
2024-05-20 21:42:50 -0400

    adding test cases to module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,6 +9,19 @@ Args: a+b
 def add_integer(a, b=98):
     """Adds two numbers after converting them to integers,
     raises TypeError if inputs are not numbers.
+    >>> add_integer(2,3)
+    5
+    >>> add_integer(5,5)
+    10
+    >>>
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(0, {})
+    Traceback (most recent call last):
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+    TypeError: a must be an integer
     """
     if isinstance(a, float):
         a = int(a)

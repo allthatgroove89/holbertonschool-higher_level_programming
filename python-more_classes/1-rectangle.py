0d30c0aaf91ba12ea8f020575eba900327a5a1fc
Your Name <saul.vera787@gmail.com>
2024-05-21 18:04:13 -0400

    Documenting Module

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,25 +11,30 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self._Rectangle__height = height
         self._Rectangle__width = width
-    """Instance attribute to retrieve value"""
+
+    """Method property"""
     @property
     def width(self):
         return self._Rectangle__width
+
     """Property setter"""
     @width.setter
     def width(self, value):
+        """sets the value of widht"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self._Rectangle__width = value
-    """Instance attribute"""
+
+    """Method property"""
     @property
     def height(self):
         return self._Rectangle__height
-    """Property setter"""
+
     @height.setter
     def height(self, value):
+        """sets the value of height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:

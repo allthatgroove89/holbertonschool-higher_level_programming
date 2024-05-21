4c43d9242eb414c0fc37a02a0e63d6d3db403184
Your Name <saul.vera787@gmail.com>
2024-05-20 22:57:23 -0400

    testing

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,12 +18,12 @@ def say_my_name(first_name, last_name=""):
     Traceback (most recent call last):
     ...
     TypeError: say_my_name() missing 1 required positional argument: 'first_name'
-    >>> say_my_name("Bob")
-    My name is Bob $
+    >>> say_my_name("Bob Lenon")
+    My name is Bob Lenon
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
-        print("My name is {:s} {:s}$".format(first_name, last_name))
+        print("My name is {:s} {:s}".format(first_name, last_name))

5848c2a5a93b39aa986b3f64c6a912ad58f5e06f
Your Name <saul.vera787@gmail.com>
2024-05-22 16:11:44 -0400

    Adding __init__ method and documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,11 @@
 
 
 class MyList(list):  # MyList class inherits list
+    """MyList class"""
+    def __init__(self):
+        """Initialize Class"""
+        super().__init__()
+
     def print_sorted(self):
         """This is a public instance method."""
         print(sorted(self))

65b0b17b3379dc96201c9f014f5d10644726a733
Your Name <saul.vera787@gmail.com>
2024-05-23 14:37:43 -0400

    importing

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,8 +34,8 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for num in row:
             if not isinstance(num, (int, float)):
-                raise TypeError("matrix must be a matrix\
-                    (list of lists) of integers/floats")
+                raise TypeError(
+                  "matrix must be a matrix (list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:

e3ffde1a2e8ba25df7b80161fbacd4c170a05092
Your Name <saul.vera787@gmail.com>
2024-05-22 20:36:44 -0400

    task 7-11

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 """
 BaseGeometry class
+Public instance method: def area(self):
+    that raises an Exception with the message
+    area() is not implemented
+    Public instance method:
+    def integer_validator(self, name, value):
+        that validates value:
+        you can assume name is always a string
+        if value is not an integer: raise a TypeError
+        exception, with the message <name> must be an integer
+        if value is less or equal to 0: raise a ValueError
+        exception with the message <name> must be greater
+        than 0
 """
 
 
@@ -12,10 +24,8 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validate integer value
-        """
+        """Validate integer value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
-"""
-BaseGeometry class
-"""
-
-
-class BaseGeometry:
-    """BaseGeometry"""
-    def area(self):
-        """Raise exception if the area
-        of a shape is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate integer value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

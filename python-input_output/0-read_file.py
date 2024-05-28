532de9ebd44eef1c0f242566362055f5e34ecb72
Your Name <saul.vera787@gmail.com>
2024-05-28 15:17:05 -0400

    Python - Input/Output directory and files

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a function that reads
+a file and prints its content to stdout.
+"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints its content to stdout.
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read())

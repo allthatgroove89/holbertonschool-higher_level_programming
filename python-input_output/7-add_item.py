f4d469eabd685d3f2701cb9dd54ac71263f50377
Your Name <saul.vera787@gmail.com>
2024-05-28 17:27:45 -0400

    adding exceptions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,8 +10,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+try:
+    my_list = load_from_json_file(filename)
+except (FileNotFoundError):
+    my_list = []
 
-my_list = []
 for i in argv[1:]:
     my_list.append(i)
 

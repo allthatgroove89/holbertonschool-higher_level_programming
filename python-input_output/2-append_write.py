49342b267c3b95647eb9a63cd138ac9b5c0e4b2a
Your Name <saul.vera787@gmail.com>
2024-05-28 15:56:44 -0400

    appends string and returns num of characters added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains a function that appends
+a string to a text file and returns the number of
+characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to text file
+    and returns len
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        return len(text)

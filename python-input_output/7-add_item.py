c270935658f542a7eb0b585e8ac61eec8f5c637a
Your Name <saul.vera787@gmail.com>
2024-05-28 17:28:36 -0400

    adding exceptions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,7 @@ and then saves them to a file."""
 
 
 from sys import argv
-
+import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -12,7 +12,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 try:
     my_list = load_from_json_file(filename)
-except (FileNotFoundError):
+except (FileNotFoundError,json.JSONDecodeError):
     my_list = []
 
 for i in argv[1:]:

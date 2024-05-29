53b7820a6e7c9cb8943482c87ca4821e4e1c5c04
Your Name <saul.vera787@gmail.com>
2024-05-29 18:00:14 -0400

    returning result of pickle.load instead of None

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -4,19 +4,19 @@ import pickle
 
 
 class CustomObject:
-    def __init__(self, Name, age, is_student):
-        self.Name = "John"
+    def __init__(self, name, age, is_student):
+        self.name = "John"
         self.age = 25
         self.is_student = True
 
     def __str__(self):
-        return f"CustomObject: {self.Name}, {self.age}, {self.is_student}"
+        return f"CustomObject: {self.name}, {self.age}, {self.is_student}"
 
     def __repr__(self):
-        return f"CustomObject: {self.Name}, {self.age} {self.is_student}"
+        return f"CustomObject: {self.name}, {self.age} {self.is_student}"
 
     def display(self):
-        print(f"Name: {self.Name}\nAge: {self.age}\
+        print(f"Name: {self.name}\nAge: {self.age}\
             \nIs Student: {self.is_student}")
 
     def serialize(self, filename):
@@ -31,7 +31,7 @@ class CustomObject:
     def deserialize(cls, filename):
         try:
             with open(filename, 'rb') as input_file:
-                pickle.load(input_file)
+                return pickle.load(input_file)
         except Exception as e:
             print(f"An error occurred while deserializing the object: {e}")
             return None

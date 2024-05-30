b161c8093d90196091baf023d6066ad474ca2140
Your Name <saul.vera787@gmail.com>
2024-05-30 18:37:15 -0400

    Adding 404 error

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -19,11 +19,23 @@ class MyHttpRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(bytes("OK", "utf8"))
-        else:
+        elif self.path == '/info':
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            info = {
+                "version": "1.0", "description":
+                "A simple API built with http.server"}
+            self.wfile.write(bytes(json.dumps(info), "utf8"))
+        elif self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(bytes("Hello, this is a simple API!", "utf8"))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes("Endpoint not found", "utf8"))
 
 
 if __name__ == '__main__':

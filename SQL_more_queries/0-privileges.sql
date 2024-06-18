1db876a1dd76e9979a39bb3e573f600c009b43d7
Your Name <saul.vera787@gmail.com>
2024-06-18 17:50:16 -0400

    changing code

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- This is a script that lists all privileges of the MySQL users, user_0d_1 and user_0d_2 in Localhost.
-GRANT ALL PRIVILEGES ON MySQL.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON MySQL.* TO 'user_0d_2'@'localhost'
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';

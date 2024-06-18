f45eca813c66b9982418bea57bbebb0c064a141e
Your Name <saul.vera787@gmail.com>
2024-06-18 18:55:42 -0400

    correcting syntax

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Creates table unique-id in server
 --fields id and name
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
+    'id' INT DEFAULT 1 UNIQUE,
+    'name' VARCHAR(256) NOT NULL,
     );

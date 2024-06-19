ce1ff94be782f8281479de73506c37be2639407f
Your Name <saul.vera787@gmail.com>
2024-06-19 17:30:15 -0400

    commenting and documenting my module

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,17 +10,21 @@ if __name__ == "__main__":
     password = argv[2]
     database_name = argv[3]
     state_name_searched = argv[4]
-
+    # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database_name)
-
+    # Create a cursor object
     cur = db.cursor()
+    # Execute the SQL query
     cur.execute(
         "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
         (state_name_searched,)
     )
+    # Fetch the first row from the result set
     row = cur.fetchone()
+    # If a row was fetched, print it
     if row is not None:
         print(row)
+    # Close the cursor and the database connection
     cur.close()
     db.close()

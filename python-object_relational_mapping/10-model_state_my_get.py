f3dfed9eb6e7e92043b0fb931f88c9e45eb89192
Your Name <saul.vera787@gmail.com>
2024-06-21 14:07:26 -0400

    Get state name

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that prints the State object with the name passed as argument"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker, exc
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name_to_search = sys.argv(4)
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        states = session.query(State).filter
+        (State.name == state_name_to_search).one()
+        print(states.id)
+    except exc.NoResultFound:
+        print("Not found")

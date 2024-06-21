a5b044757021126a391e76b0968fc73101326cb0
Your Name <saul.vera787@gmail.com>
2024-06-21 17:47:21 -0400

    correcting typo

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,8 +15,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine
-    (f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()

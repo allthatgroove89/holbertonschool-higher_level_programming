cfc2a20a1bfd57102c495b67995c6d8187a3fb49
Your Name <saul.vera787@gmail.com>
2024-06-21 11:15:37 -0400

    Print first state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that prints the first State object from the database"""
+
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).all()
+    if states:
+        for state in states:
+            print(state.name)
+    else:
+        print("No states found in database")
+
+    session.close()

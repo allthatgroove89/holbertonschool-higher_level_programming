4816fc1f86a596f54cd72fd1a45788d4d08ef564
Your Name <saul.vera787@gmail.com>
2024-06-21 14:28:07 -0400

    correcting pycodestyle and EOF

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,11 +7,13 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (username, password, database))
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -19,4 +21,4 @@ if __name__ == "__main__":
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
 
-    session.close()
+session.close()

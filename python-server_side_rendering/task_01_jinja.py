b709989f4f02dc1909facf48f74aa287f457e04f
Your Name <saul.vera787@gmail.com>
2024-07-15 21:25:30 -0400

    syntax error

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -10,7 +10,7 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.rout('/contact')
+@app.route('/contact')
 def contact():
     return render_template('conctact.html')
 if __name__ == "__main__":

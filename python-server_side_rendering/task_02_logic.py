c055b0a3525f1fd8120056167a11d2bb584fca2e
Your Name <saul.vera787@gmail.com>
2024-07-15 21:45:14 -0400

    task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,22 +1,35 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
     return render_template('conctact.html')
 
+
 @app.route('/items')
 def item():
+    try:
+        with open('items.json', 'r') as file:
+            data = json.load(file)
+            items_list = data.get("items", [])
+    except FileNotFoundError:
+        items_list = []
+
     return render_template('items.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
